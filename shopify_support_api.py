f5f5a27b2f88a02641117e76411f1f8aa9881bde
Ractiveone <ractiveone@gmail.com>
2025-03-11 15:08:54 -0300

    git add shopify_support_api.py
    git commit -m "Fixed port binding for Render"

--- shopify_support_api.py
+++ shopify_support_api.py
@@ -93,6 +93,9 @@ def order_info():
 
     return jsonify({"response": chat_response})
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render nutzt eine PORT-Umgebungsvariable
+    app.run(host="0.0.0.0", port=port, debug=True)
 
